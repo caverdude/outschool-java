0ed64ee8b03351fbaf36028e40276a95b573fe42
caverdude <javacaverdude@gmail.com>
2022-08-08 19:15:11 -0500

    added constants and some planet images

--- java/applets/arrays/OneDArrays.java
+++ java/applets/arrays/OneDArrays.java
@@ -2,6 +2,7 @@
 import javax.swing.JApplet;
 import javax.swing.SwingUtilities;
 import javax.swing.JLabel;
+import java.awt.*;
 /**
  *  Example of One Dimentional Arrays, or just an Array.
  * Kyle Le
@@ -10,7 +11,15 @@ import javax.swing.JLabel;
  */
 @SuppressWarnings("deprecation")
 public class OneDArrays extends JApplet {
-    
+    final int MERCURY=0;
+    final int VENUS=1;
+    final int EARTH=2;
+    final int MARS=3;
+    final int JUPITOR=4;
+    final int URANUS=5;
+    final int NEPTUNE=6;
+    final int PLUTO=7;
+
     String[] planets = {"Mercury", "Venus", "Earth", "Mars", "Jupitor", "Uranus", "Neptune", "Pluto"};
     
     //Called when this applet is loaded into the browser.
@@ -20,16 +29,14 @@ public class OneDArrays extends JApplet {
         try {
             SwingUtilities.invokeAndWait(new Runnable() {
                 public void run() {
-
-
-
-                    JLabel lbl = new JLabel("Planet name: " + planets[0]);
+                    setLayout(new GridLayout(8,1));
+                    JLabel lbl = new JLabel("Planet name: " + planets[MERCURY]);
                     add(lbl);
-                    JLabel lbl2 = new JLabel("Planet name: " + planets[1]);
+                    JLabel lbl2 = new JLabel("Planet name: " + planets[VENUS]);
                     add(lbl2);
-                    JLabel lbl3 = new JLabel("Planet name: " + planets[2]);
+                    JLabel lbl3 = new JLabel("Planet name: " + planets[EARTH]);
                     add(lbl3);
-                    JLabel lbl4 = new JLabel("Planet name: " + planets[3]);
+                    JLabel lbl4 = new JLabel("Planet name: " + planets[MARS]);
                     add(lbl4);
                     JLabel lbl5 = new JLabel("Planet name: " + planets[4]);
                     add(lbl5);
