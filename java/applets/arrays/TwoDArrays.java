5e590c5b9be7d4e074c0b2f77d03e72a21a5c73e
caverdude <javacaverdude@gmail.com>
2022-08-24 19:40:49 -0500

    work in class

--- java/applets/arrays/TwoDArrays.java
+++ java/applets/arrays/TwoDArrays.java
@@ -1,4 +1,10 @@
-/**
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import java.awt.GridLayout;
+
+/*
  *  Example for use of two D arrays. Two Dimensioanl Arrays.
  *  
  *  - Lookup Tables for all dimentions of arrays.
@@ -18,7 +24,55 @@
  *  
  *  
  */
+@SuppressWarnings("deprecation")
+/**
+ * 
+ */
+public class TwoDArrays extends JApplet{
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    protected ImageIcon createImageIcon(String path,
+            String description) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
 
-public class TwoDArrays {
-    
+    /*
+     * "bqr","bqk","bqb","bq","bk","bkb","bkk","bkr"
+     * "bp","bp","bp","bp","bp","bp","bp","bp"
+     * " "," "," "," "," "," "," "," "
+     * 
+     */
+
+    // Called when this applet is loaded into the browser.
+    public void init() {
+        // System.out.println("test");
+        // Execute a job on the event-dispatching thread; creating this applet's GUI.
+
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    setLayout(new GridLayout(8, 8));
+                    for(int col=0;col<8;col++){
+                        for(int row=0;row<8;row++){
+                            ImageIcon wKingIcon  = createImageIcon("chess/wking.png",    "White King");
+                            //mercuryIcon = new ImageIcon(mercuryIcon.getImage().getScaledInstance(ICON_SIZE, ICON_SIZE, Image.SCALE_DEFAULT));
+                            JLabel wKing = new JLabel();
+                            wKing.setIcon(wKingIcon);
+                            //TODO:cut out the rest of the chess pieces
+                            add(wKing);
+                        }
+                    }
+                     
+                }
+            });
+        } catch (Exception e) {
+            System.err.println("createGUI didn't complete successfully");
+        }
+    }
+
 }
