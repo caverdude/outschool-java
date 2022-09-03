fd9e54b70f944a0807a95a4b1e18e497ec846bd3
caverdude <javacaverdude@gmail.com>
2022-09-02 19:36:53 -0500

    code

--- java/applets/flowcontrol/decisions/SwitchCase.java
+++ java/applets/flowcontrol/decisions/SwitchCase.java
@@ -1,3 +1,11 @@
 public class SwitchCase {
-    
+    // This is called a no args or arguments constructor method
+    // or simply constructor or no args constructor
+    public SwitchCase(){
+            
+    }
+    public static void main(String args[]){
+        // use of new keyword is a creational pattern called prototype
+        new SwitchCase();
+    }
 }
