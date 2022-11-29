587de7fef8b11e31998863c62bce2e33ac64f6d4
caverdude <javacaverdude@gmail.com>
2022-11-28 18:59:15 -0600

    Adding Exception code

---
+++ java/exceptions/CauseError.java
@@ -0,0 +1,32 @@
+import javax.swing.JOptionPane;
+
+public class CauseError {
+
+    public static void main(String args[]){
+        // try the folowing with .methodA() and .methodC()
+        new CauseError().methodC();
+    }
+
+    public void methodA(){
+        methodB();
+    }
+    public void methodB(){
+        methodA();
+    }
+
+    public void methodC(){
+        try{
+            methodA();
+        }catch(StackOverflowError sofe){
+            // try this with the following line commented out
+            // you will see no output at all, the program just stops
+            //sofe.printStackTrace();
+            // try the toString()
+            //System.out.println(""+sofe.toString());
+            // try a custom text message
+            //System.out.println("Stack Overflow Error!!!!! Program halted.");
+            // try GUI message box
+            //JOptionPane.showMessageDialog(null, "Stack Overflow Error!!!!! Program halted.");
+        }
+    }
+}
---
+++ java/exceptions/JavaProperties.java
@@ -0,0 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class JavaProperties {
+    private static Properties properties;
+    public static void main(String[] args){
+        try{
+            properties = new Properties();
+            // rename this .txt or .prop to see the issues
+            properties.load(new FileReader("properties.prop"));
+
+        }catch(FileNotFoundException fnfe){
+            fnfe.printStackTrace();
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        } // more general exceptions last.
+
+        // try the above by commenting out FileNotFoundExcpetion  and then the IOExeptoin 
+        // and see the various output.
+
+        properties.list(System.out);
+    }
+}
